878cdbab7fa752c64ebb78c46455e80450cdd779
jm-janzen <jmjanzen@gmail.com>
2016-01-31 14:10:16 +0000

    Greatly reduce complexity of server and repository
    *	wrestle back control from express by using ejs
    	to serve files.
    *	add package.json and remove node_modules folder
    	from repository.  That information is in
    	package.json, anyway.
    *	fix problem where css files were not being being
    	encoded with the appropriate Content-type.

--- facer.js
+++ facer.js
@@ -3,10 +3,7 @@
 /* TODO
  *   write log module to write and print
  *   add countdown timer
- *   add CSS
- *   fix missing content-type text/css
  */
-
 var http = require('http')
   , fs   = require('fs')
   , log = console.log
@@ -15,6 +12,21 @@ var http = require('http')
 var express = require('express');
 var app     = express();
 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(express.static(__dirname + '/views'));
+app.get('/', function (req, res) {
+	res.render('index.html');
+});
+
+app.listen(PORT, function () {
+	console.log('facer listening on port', PORT);
+});
+
+/*
+ * restart server on file change
+ */
 var forever = require('forever-monitor');
 var child = new (forever.Monitor)('facer.js', {
 	max: 3,
@@ -29,29 +41,5 @@ child.on('watch:restart', function (info) {
 });
 child.on('restart', function () {
 	log('restarting server for %s time', child.times);
-});
-
-http.createServer(function (req, res) {
-	var dirName = fs.realpathSync('.');
-	var path = req.url;
-	console.log('req:', path);
-	if (path.indexOf('/') != -1) {
-		fs.readFile(dirName + '/views/index.html', function (error, html) {
-			if (error) log('Error loading html:', error);
-			res.writeHeader(200, {'Content-Type' : 'text/html' });
-			res.write(html);
-			res.end();
-		});
-	}
-	if (path.indexOf('/views/style.css') != -1) {
-		fs.readFile(dirName + '/views/style.css', function (error, css) {
-			if (error) log('Error loading css:', error);
-			res.writeHeader(200, {'Content-Type' : 'text/css' });
-			res.write(css);
-			res.end();
-		});
-	}
-}).listen(PORT, function () {
-	log('server listening on port', PORT);
 });
 
