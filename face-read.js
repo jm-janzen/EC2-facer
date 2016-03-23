b44c58ba61808077ae22d1e4e3030d0aeb84c97a
jm-janzen <jmjanzen@gmail.com>
2016-03-23 10:43:13 -0500

    Split up assignment logic and add elucidating comment
    This is the first in a line of commit intended to make this module more useable

--- face-read.js
+++ face-read.js
@@ -11,14 +11,16 @@ exports.read = function (path, type, callback) {
 
     var textFiles = {};
     var regex = new RegExp("\\." + type);
+    var typeLen = (type.length * -1) -1;
 
     fs.readdir(path, function (error, files) {
         if (error) throw new Error("Error reading from path: " + path);
 
         for (var file = 0; file < files.length; file++) {
             if (files[file].match(regex)) {
+                // load textFiles with content
                 textFiles[files[file]
-                  .slice(0, (type.length * -1) - 1 )] = fs.readFileSync(path
+                  .slice(0, typeLen] = fs.readFileSync(path
                     + '/'
                     + files[file]
                     , 'utf8');
