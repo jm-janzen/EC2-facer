cc250fb4187f6ddefab7e134ec955c2325ca6a3b
jm-janzen <jmjanzen@gmail.com>
2016-06-07 18:21:36 -0500

    Update start script to be a bit more intelligent
    This will relate to eventual changes in using a variable port in the server proper

--- start.sh
+++ start.sh
@@ -1,9 +1,19 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 # check for forever module
 ls /usr/local/lib/node_modules | grep 'forever' 1>/dev/null \
     || $(echo 'Install foreverjs globally (`npm install -g forever`) to start this script.' && exit 1;)
 
-echo 'Starting facer . . .'
-forever -w --minUptime 1000 --spinSleepTime 1000 facer.js $1
+if [[ $# -lt 2 ]]; then
+    cat <<EOF
+    Usage: ./start.sh <PORT> <TOKEN>
+EOF
+    exit 1
+else
+    ### TODO validate port & token parms
+
+    echo "Starting facer.js on port $1"
+
+    forever -w --minUptime 1000 --spinSleepTime 1000 facer.js $1 $2
+fi
 
